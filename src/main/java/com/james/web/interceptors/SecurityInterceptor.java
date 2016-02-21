0b6e9bc44ce4f4f6eb390946ffe896c837dcf81e
2517594@qq.com <ch1987hzgit>
2016-02-21 11:32:25 +0800

    拦截器的使用

---
+++ src/main/java/com/james/web/interceptors/SecurityInterceptor.java
@@ -0,0 +1,38 @@
+package com.james.web.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SecurityInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+	{
+		System.err.println("SecurityInterceptor拦截器：preHandle");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.err.println("SecurityInterceptor拦截器：postHandle");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.err.println("SecurityInterceptor拦截器：afterCompletion");
+		
+	}
+
+/*	@Override
+	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		super.afterConcurrentHandlingStarted(request, response, handler);
+	}*/
+
+}
